b5a211b5de72499c08629758387ac7aa3d9fb02f
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2023-01-19 10:06:54 +0300

    Add files via upload

---
+++ Matevosyan_10_17/Matevosyan_10_17/Program.cs
@@ -0,0 +1,12 @@
+﻿float c = (2 * Fact(5) + 3 * Fact(8)) / (Fact(6) + Fact(4));
+Console.WriteLine(c);
+
+static int Fact(int n)
+{
+    int i, f = 1; 
+    for (i = 1; i <= n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
